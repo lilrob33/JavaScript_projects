20b75a77844bbe62e2d16fbcaa921238c078e033
lil_rob_33 <48527902+lilrob33@users.noreply.github.com>
2020-01-04 20:01:47 -0800

    add basic_html.html and js folder to repo

---
+++ js/basic_javascript_1.js
@@ -0,0 +1,13 @@
+var x = "The Seattle Seahawks, "
+
+var b = "As Oscar Wilde said, \"Experience is the name everyone gives to thier mistakes\""
+
+var c = x + b
+
+family = "Suarez", dad = "Pedro", mom = "Laureen", daughter  = "Ariana";
+
+var d = x + b + c + daughter
+var d = d.fontcolor("blue");
+document.write(d);
+
+document.write(6 + 6);
