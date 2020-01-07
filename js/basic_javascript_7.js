210fa8643771478c2356ab8f80559a1a937671ef
lil_rob_33 <48527902+lilrob33@users.noreply.github.com>
2020-01-06 17:20:51 -0800

    add basic_javascript_7.js file to repo

---
+++ js/basic_javascript_7.js
@@ -0,0 +1,15 @@
+var time1 = new Date().getHours();
+
+function time_func() {
+    var greeting;
+    if (time1 < 10 == time1 > 0) {
+        greeting = "It's morning time. Ugh";
+    }
+        else if (10 > time1 == 14 < time1) {
+        greeting = "How is work going today? It's almost over now.";
+    }
+    else {
+        greeting = "It's Miller time!!";
+    }
+    document.getElementById("time1").innerHTML = greeting;
+}
