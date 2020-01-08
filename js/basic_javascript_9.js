f33764794a7791e23a3862cfdfd068e32b098e83
lil_rob_33 <48527902+lilrob33@users.noreply.github.com>
2020-01-07 18:50:29 -0800

    add basic_javascript_9.js file to repo

---
+++ js/basic_javascript_9.js
@@ -0,0 +1,28 @@
+var slideIndex = 1;
+showslides(slideIndex);
+
+// Next/prev controls
+function plusSlides(n) {
+  showslides(slideIndex += n);
+}
+
+// image controls
+function currentslide(n) {
+  showslides(slideIndex = n);
+}
+
+function showslides(n) {
+    var i;
+    var slides = document.getElementsByClassName("slides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+  } 
