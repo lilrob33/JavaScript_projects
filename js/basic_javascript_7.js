e9b78008367fd5ddde4dc21ba8ac0a9cdb0bee74
lil_rob_33 <48527902+lilrob33@users.noreply.github.com>
2020-01-08 19:16:42 -0800

    change == to && lines 5&8 to basic_javascript_7.js
    Also had <> signs backwards

--- js/basic_javascript_7.js
+++ js/basic_javascript_7.js
@@ -2,10 +2,10 @@ var time1 = new Date().getHours();
 
 function time_func() {
     var greeting;
-    if (time1 < 10 == time1 > 0) {
+    if (time1 < 10 && time1 > 0) {
         greeting = "It's morning time. Ugh";
     }
-        else if (10 > time1 == 14 < time1) {
+        else if (10 < time1 && 14 > time1) {
         greeting = "How is work going today? It's almost over now.";
     }
     else {
